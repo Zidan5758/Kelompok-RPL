227cb7ae495c6a180b60d73a55e468eb227daf30
helmyfl <helmyefel@students.unnes.ac.id>
2021-06-14 11:33:18 +0700

    Revert "Progress Final"
    This reverts commit 20fb5585da8f350baadfd963ce2fd9f73b63e152.

--- application/views/cms/manage/menu_grup.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
-
-<script type='text/javascript'>
-	$(function(){
-		load_silent('cms/manage/show_menu_grup/','#divsubcontent');
-	});
-</script>
-
-<div id='divsubcontent'></div>
--- application/views/utility/js_exec.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');?>
-
-<script type='text/javascript'>
-    $(function(){
-        <?php echo $js;?>
-    });
-</script>
